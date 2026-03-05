b99a0117c89a4547d9feb7d3b2a3d008fd22b18e
Gabriel Leal <114974022+gabslealdev@users.noreply.github.com>
2026-03-05 17:05:55 -0300

    chore: foundation .NET e Angular (#3)
    * feat(api): adiciona swagger e endpoint de healthcheck
    
    * chore: cria esqueleto inicial (angular web)

---
+++ src/web/stackforge-web/src/app/features/mentor/ui/mentor-register-page/mentor-register-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mentor-register-page',
+  imports: [],
+  templateUrl: './mentor-register-page.html',
+  styleUrl: './mentor-register-page.css',
+})
+export class MentorRegisterPage {
+
+}
