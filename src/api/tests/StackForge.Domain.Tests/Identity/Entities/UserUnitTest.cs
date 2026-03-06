8c02bf6a6665bb47a7f454ae8fd363d874fbc627
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-06 14:51:47 -0300

    Implementa teste de unidade para entidade User

--- src/api/tests/StackForge.Domain.Tests/Identity/Entities/UserUnitTest.cs
+++ src/api/tests/StackForge.Domain.Tests/Identity/Entities/UserUnitTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bogus;
+using Shouldly;
+using StackForge.Domain.Identity.Enums;
+using StackForge.Domain.Tests.Identity.Builders;
 
 namespace StackForge.Domain.Tests.Identity.Entities
 {
-    internal class UserUnitTest
+    public class UserUnitTest
     {
+        [Fact]
+        public void ShouldCreate_WhenIsValid()
+        {
+            // arrange 
+            var faker = new Faker();
+            var email = faker.Internet.Email();
+            var passwordHash = faker.Random.Hash();
+            var role = faker.PickRandom<Role>();
+
+            // act
+            Action action = () => new UserDataBuilder().WithEmail(email).WithPasswordHash(passwordHash).WithRole(role).Build();
+
+            // assert
+            action.ShouldNotThrow();
+        }
     }
 }
