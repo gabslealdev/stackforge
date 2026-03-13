dd063ae1f2ffb4e9b92d25281ae40ad9d01a1485
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-13 19:38:51 -0300

    feat(profile): adiciona persistência do registro de mentor

---
+++ src/api/src/StacKForge.Infrastructure/Data/Mappings/Profile/LearnerMap.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StackForge.Domain.Profile.Entities;
+
+namespace StackForge.Infrastructure.Data.Mappings.Profile
+{
+    public sealed class LearnerMap : IEntityTypeConfiguration<LearnerProfile>
+    {
+        public void Configure(EntityTypeBuilder<LearnerProfile> builder)
+        {
+            builder.ToTable("learners");
+
+            builder.HasKey(l => l.Id)
+                .HasName("PK_learner");
+
+            builder.HasIndex(l => l.UserId)
+                .IsUnique();
+
+            builder.OwnsOne(l => l.Name, name =>
+            {
+                name.Property(n => n.FirstName)
+                .HasColumnName("first_name")
+                .HasMaxLength(80)
+                .IsRequired();
+
+                name.Property(n => n.LastName)
+                .HasColumnName("last_name")
+                .HasMaxLength(80)
+                .IsRequired();
+            });
+
+            builder.Property(l => l.UserId)
+                .ValueGeneratedNever()
+                .IsRequired();
+
+            builder.Property(l => l.BirthDate)
+                .HasColumnType("date")
+                .HasColumnName("birth_date")
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .HasColumnType("timestamp with time zone")
+                .HasColumnName("created_at")
+                .IsRequired();
+
+        }
+    }
+}
---
+++ src/api/src/StacKForge.Infrastructure/Data/Repositories/Profile/MentorProfileRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackForge.Application.Profile.Interfaces;
+using StackForge.Domain.Profile.Entities;
+using StackForge.Infrastructure.Data.Context;
+
+namespace StackForge.Infrastructure.Data.Repositories.Profile
+{
+    internal class MentorProfileRepository : IMentorProfileRepository
+    {
+        private readonly StackForgeDbContext _context;
+
+        public MentorProfileRepository(StackForgeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(MentorProfile mentorProfile)
+        {
+            await _context.Mentors.AddAsync(mentorProfile);
+        }
+
+        public async Task<bool> ExistsByUserIdAsync(Guid userId)
+        {
+            return await _context.Mentors.AnyAsync(x => x.UserId == userId);
+        }
+    }
+}
