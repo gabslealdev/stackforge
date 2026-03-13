4cef202009e229d4e56fb97b38c0c5d87e0528db
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-13 19:38:21 -0300

    refactor(infrastructure): reorganiza repositórios de identity

---
+++ src/api/src/StacKForge.Infrastructure/Data/Repositories/Identity/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackForge.Application.Identity.Interfaces.Repository;
+using StackForge.Domain.Identity.Entities;
+using StackForge.Domain.Identity.ValueObjects;
+using StackForge.Infrastructure.Data.Context;
+
+namespace StackForge.Infrastructure.Data.Repositories.Identity
+{
+    public sealed class UserRepository : IUserRepository
+    {
+        private readonly StackForgeDbContext _context;
+
+        public UserRepository(StackForgeDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public async Task<bool> ExistsByEmailAsync(Email email)
+        {
+            return await _context.Users.AnyAsync(user => user.Email == email);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
+        }
+    }
+}
