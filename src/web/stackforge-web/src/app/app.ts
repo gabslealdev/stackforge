afee4aa41182bc8e75d0ae3963a74d262e0c368c
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-15 19:49:12 -0300

    feat: estrutura base do frontend com layout, rotas e estilos globais

--- src/web/stackforge-web/src/app/app.ts
+++ src/web/stackforge-web/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header.component/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
