79463e6be05c484bfbc4f02971ccc2993d00b50c
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-15 19:50:46 -0300

    fix: configura cors para integração com frontend Angular

--- src/api/src/StackForge.Api/Program.cs
+++ src/api/src/StackForge.Api/Program.cs
@@ -8,14 +8,30 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-var app = builder.Build();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("StackForgeWeb", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:4200",
+                "https://localhost:4200"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("StackForgeWeb");
+
 
 app.MapControllers();
 
